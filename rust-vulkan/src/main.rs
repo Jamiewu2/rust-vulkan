e9c16413968024772fe488de552070d12f0a5a61
Jamie Wu <jwu@vertafore.com>
2019-09-16 14:43:46 -0400

    Added vulkan instance creation

--- rust-vulkan/src/main.rs
+++ rust-vulkan/src/main.rs
@@ -2,6 +2,8 @@ use winit::EventsLoop;
 use winit::WindowBuilder;
 use winit::{Event, WindowEvent};
 use winit::dpi::LogicalSize;
+use vulkano::instance::{Instance, InstanceExtensions, ApplicationInfo, Version};
+use std::sync::Arc;
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
@@ -9,6 +11,7 @@ const HEIGHT: u32 = 600;
 // a rust struct is basically a Kotlin data class, or more generally a named Tuple
 #[allow(unused)]
 struct HelloTriangleApp {
+    instance: Arc<Instance>,
     events_loop: EventsLoop,
 }
 
@@ -16,9 +19,11 @@ struct HelloTriangleApp {
 impl HelloTriangleApp {
     //capital Self = type, HelloTriangleApp in this case
     fn init() -> Self {
+        let instance = Self::init_instance();
         let events_loop = Self::init_window();
 
         Self {
+            instance,
             events_loop
         }
     }
@@ -31,6 +36,23 @@ impl HelloTriangleApp {
             .build(&event_loop)
             .unwrap();
         return event_loop;
+    }
+
+    fn init_instance() -> Arc<Instance> {
+        let supported_extensions = InstanceExtensions::supported_by_core()
+            .expect("failed to retrieve supported extensions");
+        println!("Supported extension: {:?}", supported_extensions);
+
+        let app_info = ApplicationInfo {
+            application_name: Some("Hello Triangle".into()),
+            application_version: Some(Version { major: 1, minor: 0, patch: 0 }),
+            engine_name: Some("No Engine".into()),
+            engine_version: Some(Version { major: 1, minor: 0, patch: 0 }),
+        };
+
+        let required_extensions = vulkano_win::required_extensions();
+        Instance::new(Some(&app_info), &required_extensions, None)
+            .expect("failed to create Vulkan instance")
     }
 
     //&mut self = self: &mut Self
